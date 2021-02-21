84c267c70168c845af8ced79b0974f1ea35f316f
Chris <christopher.ca.adams@gmail.com>
2021-02-21 15:13:40 +0100

    TypeScript WebGL Game Engine Tutorial #04 - A Triangle

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -8,6 +8,8 @@ export class Engine {
     private _canvas: HTMLCanvasElement;
     private _shader: Shader;
 
+    private _buffer: WebGLBuffer;
+
     public constructor() {
         console.log('Engine created.');
     }
@@ -16,6 +18,8 @@ export class Engine {
         if (this._canvas) {
             this._canvas.width = window.innerWidth;
             this._canvas.height = window.innerHeight;
+
+            gl.viewport(0, 0, this._canvas.width, this._canvas.height);
         }
     }
 
@@ -28,6 +32,8 @@ export class Engine {
         this.loadShaders();
         this._shader.use();
 
+        this.createBuffer();
+
         this.loop();
         return this;
     }
@@ -35,10 +41,33 @@ export class Engine {
     private loop(): void {
         gl.clear(gl.COLOR_BUFFER_BIT); // ??? What is this? Resetting everything, but how?
 
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
         requestAnimationFrame(() => {
             
             this.loop();
         });
+    }
+
+    private createBuffer(): void {
+        this._buffer = gl.createBuffer();
+
+        const vertices = [
+            0,   0,   0,
+            0,   0.5, 0,
+            0.5, 0.5, 0
+        ];
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(0);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, undefined);
+        gl.disableVertexAttribArray(0);
     }
 
     private loadShaders(): void {
